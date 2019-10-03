342c187e6ea968e3fc21f356361ea7dc293d7e24
Robbie Malone <robmalone7@gmail.com>
2019-10-03 19:34:52 +1000

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,96 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app style="background:#efefef">
+    <!-- <v-navigation-drawer v-model="drawer" absolute right temporary color="black">
+      <v-list nav dense>
+        <v-list-item-group v-model="group" active-class="deep-purple--text text--accent-4">
+          <v-list-item>
+            <v-list-item-title>Foo</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Bar</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Fizz</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-title>Buzz</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>-->
+
+    <v-content>
+      <HelloWorld />
+    </v-content>
+    <v-footer class="white" padless>
+      <v-container>
+        <v-card class="flex" flat tile>
+          <!-- <v-card-title class="white"></v-card-title> -->
+          <v-layout>
+            <v-flex md2>
+              <ul>
+                <li class="heading">INFO</li>
+                <br />
+                <li>About</li>
+                <li>Journal</li>
+                <li>Contact</li>
+              </ul>
+            </v-flex>
+            <v-flex md2>
+              <ul>
+                <li class="heading">CATEGORIES</li>
+                <br />
+                <li>Wood</li>
+                <li>Fashion</li>
+                <li>UX Stuff</li>
+              </ul>
+            </v-flex>
+            <v-flex md2>
+              <ul>
+                <li class="heading">FOLLOW</li>
+                <br />
+                <li style="color: blue">Facebook</li>
+                <li>Instagram</li>
+                <li style="color:teal">Twitter</li>
+                <li style="color:red">Pinterest</li>
+              </ul>
+            </v-flex>
+          </v-layout>
+        </v-card>
+      </v-container>
+    </v-footer>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  },
+  data: () => ({
+    drawer: false,
+    group: null
+  }),
+  watch: {
+    group() {
+      this.drawer = false;
+    }
+  }
+};
+</script>
+<style  scoped>
+ul {
+  list-style-type: none;
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.heading {
+  font-family: "Courier New", Courier, monospace;
+  color: #aca9a9;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,57 +1,209 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <div class="jumbotron">
+      <h1 style="margin-top:-20px; letter-spacing:8px; color:white; font-size: 2vw;">
+        <img src="https://img.icons8.com/ios/50/FFFFFF/maple-leaf.png" />MOKUZAI
+      </h1>
+      <v-container>
+        <h1
+          style="margin-top:80px; letter-spacing:3px; color:white; font-size: 2vw;"
+        >Great design is elimination all unnecessary details</h1>
+        <h1
+          style="margin-bottom: 150px;letter-spacing:3px; color:white; font-size: 1vw;"
+        >-MINH D. TRAN</h1>
+      </v-container>
+    </div>
+
+    <v-container>
+      <v-layout justify-center row>
+        <v-btn style="margin-bottom:50px; margin-top:-40px" text>MOST POPULAR</v-btn>
+        <v-btn style="margin-bottom:50px; margin-top:-40px" text>LOWEST PRICE</v-btn>
+        <v-btn style="margin-bottom:50px; margin-top:-40px" text>HIGHEST PRICE</v-btn>
+      </v-layout>
+      <div class="block">
+        <v-layout justify-center row wrap class="items">
+          <v-flex xs10 sm8 md3 v-for="item in ITEMS" :key="item.price">
+            <v-container>
+              <v-card class="cardMargin" flat color="white">
+                <v-container fluid>
+                  <v-layout row>
+                    <v-flex xs12 sm10 md12>
+                      <v-img :src="item.image"></v-img>
+                    </v-flex>
+                  </v-layout>
+                </v-container>
+              </v-card>
+              <div>
+                <v-card-text>
+                  {{item.title}}
+                  <br />
+                  <span class="price">$ {{item.price}}</span>
+                </v-card-text>
+              </div>
+            </v-container>
+          </v-flex>
+        </v-layout>
+      </div>
+      <v-layout justify-center row>
+        <v-btn id="btn" v-on:click="click()" x-small text>1</v-btn>
+        <v-btn id="btn" x-small text>2</v-btn>
+        <v-btn id="btn" x-small text>3</v-btn>
+        <v-btn id="btn" x-small text>4</v-btn>
+      </v-layout>
+    </v-container>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    ITEMS: [
+      {
+        title: "Walnut Speakers & Amp",
+        price: 599,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fwalnut_speakers.png?alt=media&token=d93fffb6-934f-4e5b-894f-53dd76f12758"
+      },
+      {
+        title: "Maple Lamp",
+        price: 109,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fmaple_lamp.png?alt=media&token=dbf69300-4590-4490-ab14-82c901b50e9f"
+      },
+      {
+        title: "Walnut Planter",
+        price: 39,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fwalnut_planter.png?alt=media&token=b7692959-ef86-474d-8166-5db596de1c0a"
+      },
+      {
+        title: "M18 Chair, Grey Wool Felt",
+        price: 330,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fm18_chair.png?alt=media&token=e85dfda9-3354-4917-8b29-5fc10967ac79"
+      },
+      {
+        title: "Pliot Stool, Black",
+        price: 265,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fpilot_stool.png?alt=media&token=e2dde4e8-60ba-476a-b208-4d5d2e7e96e5"
+      },
+      {
+        title: "B&O BeoPlay A9 - Rose Gold",
+        price: 2999,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fbeoplay.png?alt=media&token=d5529aa4-0489-4b2b-987d-6ae27c9391bf"
+      },
+      {
+        title: "Joey Roth CRM-001 Ceramic",
+        price: 495,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fjoey_roth.png?alt=media&token=d4a1b728-e23d-41e6-bee2-7e186ba99d78"
+      },
+      {
+        title: "V5BT Bamboo Speaker",
+        price: 200,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fv5bt_bamboo.png?alt=media&token=9427be8b-ed72-4707-91bb-00a78f755c4f"
+      },
+      {
+        title: "Walnut iPhone Case",
+        price: 79,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fwalnut_iphone.png?alt=media&token=582a2057-44c8-4188-a4c8-18ed150989df"
+      },
+      {
+        title: "Walnut Watch (Round)",
+        price: 239,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fwalnut_watch.png?alt=media&token=7b6cff3c-6a0e-4249-bebb-d3bb1978bc70"
+      },
+      {
+        title: "Polk Audio Wireless",
+        price: 705,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fpolk_audio.png?alt=media&token=bf4f68f5-e9c6-4a75-bc35-aacb55986a63"
+      },
+      {
+        title: "Zebra Series Varberg Table",
+        price: 89,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fzebra_series.png?alt=media&token=92a63799-6e87-44ce-8c15-68f3d160d3be"
+      },
+      {
+        title: "POTS Pedestal Set",
+        price: 67,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fpots_pedestal.png?alt=media&token=1b21646c-5d4d-41d7-8cf0-212e1a5dab0d"
+      },
+      {
+        title: "SoundLink Bluetooth Mobile",
+        price: 300,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fsoundlink_bluetooth.png?alt=media&token=5d8029fc-1760-4b22-b293-9ac29eb3d251"
+      },
+      {
+        title: 'Wall Clock OJ Black 31.5"',
+        price: 100,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fwall_clock.png?alt=media&token=41a3acb0-6b08-4146-9706-f22ac05905c0"
+      },
+      {
+        title: "Cherry Wood Graph 3-Pack",
+        price: 10,
+        image:
+          "https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fcherry_wood.png?alt=media&token=a70a9ac4-21a1-4933-8a41-f214debcff57"
+      }
+    ]
+  }),
+  methods: {
+    click() {
+      let button = document.getElementById("btn");
+      button.style.borderBottom = "1px";
+      // button.style.border = "solid";
+    }
+  }
+};
+</script>
+<style scoped>
+.jumbotron {
+  background: white;
+  background-image: url(https://firebasestorage.googleapis.com/v0/b/production2hats.appspot.com/o/studentPortal%2Fassessment-web-design%2Fhero.png?alt=media&token=83f29b93-3c2e-4e50-8641-0eda16a0d960);
+  /* background-size: cover; */
+  width: 100%;
+  height: auto;
+  border: 20px;
+  border-style: solid;
+  border-color: white;
+  /* max-width: 100%; */
+  /* margin: 0 auto; */
+}
+
+@media screen and (max-width: 700px) {
+  .cardMargin {
+    margin-bottom: 2px;
+    border-radius: 20px;
   }
 }
-</script>
+.push-spaces {
+  height: 500px;
+  width: 100%;
+}
+@media screen and (max-width: 700px) {
+  .block {
+    width: 300px;
+    margin: auto;
+  }
+}
+@media screen and (min-width: 800px) {
+  .block {
+    width: 850px;
+    margin: auto;
+  }
+}
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.price {
+  font-family: "Courier New", Courier, monospace;
+  color: #7e7e7e;
 }
 </style>
